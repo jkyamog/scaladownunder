25f161715e68db2355c576845856923a8139da65
Jun Yamog <jkyamog@gmail.com>
2014-09-01 00:03:09 +1200

    better writer

--- app/org/scaladownunder/PersonController.scala
+++ app/org/scaladownunder/PersonController.scala
@@ -13,6 +13,48 @@ import scala.util._
 
 import Defs._
 
+object PersonController extends Controller with DatomicDB {
+  import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+  import Person._
+
+  def writeToDb(id: Long) = Action.async(parse.json) { request =>
+    doDb {
+      writerPersonToDb(request.body) andThen
+        transact andThen
+        getEntity(id)
+    } map { entity =>
+      Ok(Json.toJson(entity))
+    }
+  }
+
+
+  def writerPersonToDb(json: JsValue): DBWriter = { db =>
+    val id = DId(Partition.USER)
+
+    readsPerson.reads(json) match {
+      case JsSuccess((fName, sName), _) =>
+        Success((id, Seq(
+          (SchemaEntity.newBuilder
+            +?= (firstName -> fName)
+            +?= (surname -> sName)
+            ) withId id
+        )))
+      case JsError(e) => Failure(new IllegalArgumentException(JsError.toFlatJson(e).toString))
+    }
+  }
+
+  val readsPerson = (
+    (__ \ 'firstName).readNullable[String] and
+    (__ \ 'surname).readNullable[String]).tupled
+
+  implicit val writesPerson = (
+    (__ \ 'firstName).writeNullable[String] and
+    (__ \ 'surname).writeNullable[String]
+  )(unlift(unapply))
+
+}
+
 
 trait DatomicDB {
   self: Controller =>
@@ -51,56 +93,13 @@ trait DatomicDB {
 }
 
 
-object PersonController extends Controller with DatomicDB {
-  import play.api.libs.concurrent.Execution.Implicits.defaultContext
-
-  import Person._
-
-  def writeToDb(id: Long) = Action.async(parse.json) { request =>
-    doDb {
-      writerPersonToDb(request.body) andThen
-      transact andThen
-      getEntity(id)
-    } map { entity =>
-      Ok(Json.toJson(writesPerson.writes(Person.unapply(entity))))
-    }
-  }
-
-
-  def writerPersonToDb(json: JsValue): DBWriter = { db =>
-    val id = DId(Partition.USER)
-
-    readsPerson.reads(json) match {
-      case JsSuccess((fName, sName), _) =>
-        Success((id, Seq(
-          (SchemaEntity.newBuilder
-            +?= (firstName -> fName)
-            +?= (surname -> sName)
-            ) withId id
-        )))
-      case JsError(e) => Failure(new IllegalArgumentException(JsError.toFlatJson(e).toString))
-    }
-  }
-
-  val readsPerson = (
-    (__ \ 'firstName).readNullable[String] and
-    (__ \ 'surname).readNullable[String]).tupled
-
-  val writesPerson = (
-    (__ \ 'firstName).writeNullable[String] and
-    (__ \ 'surname).writeNullable[String]
-  ).tupled
-
-}
-
-
 object Person {
   val ns = new Namespace("person")
 
   val firstName = Attribute(ns / "firstname", SchemaType.string, Cardinality.one)
   val surname = Attribute(ns / "surname", SchemaType.string, Cardinality.one)
 
-  def unapply(entity: Entity): (Option[String], Option[String]) = {
-    (entity.readOpt(firstName), entity.readOpt(surname))
+  def unapply(entity: Entity): Option[(Option[String], Option[String])] = {
+    Some(entity.readOpt(firstName), entity.readOpt(surname))
   }
 }
